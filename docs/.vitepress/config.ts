0804d84ed179452fa7edcb8d5e9c3e3dcad640e0
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-05-18 01:38:34 -0300

    Subindo configurações para o deploy no Github Pages.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+
+  base: '/SIA/',   // Para deploy no Github Pages
+
   title: "SIA - Scalable infrastructure for applications",
   description: "Tutorial de criação de uma infraestrutura escalonável para aplicações na AWS",
 
